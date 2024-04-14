df3ea987cb691667f06da351ed6b689f2b0a0c15
ventiv 111 <1841013744@qq.com>
2024-04-14 09:48:29 +0000

    4

---
+++ hello_console/lib/leiyuduixiang/jicheng.dart
@@ -0,0 +1,38 @@
+class Television {
+  void turnOn() {
+    _illuminateDisplay();
+    _activateIrSensor();
+  }
+  // ···
+}
+
+class SmartTelevision extends Television {
+  void turnOn() {
+    super.turnOn();
+    _bootNetworkInterface();
+    _initializeMemory();
+    _upgradeApps();
+  }
+  // ···
+}
+
+class Television {
+  // ···
+  set contrast(int value) {...}
+}
+
+class SmartTelevision extends Television {
+  @override
+  set contrast(num value) {...}
+  // ···
+}
+
+class A {
+  // Unless you override noSuchMethod, using a
+  // non-existent member results in a NoSuchMethodError.
+  @override
+  void noSuchMethod(Invocation invocation) {
+    print('You tried to use a non-existent member: '
+        '${invocation.memberName}');
+  }
+}
