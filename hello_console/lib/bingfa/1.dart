03f3b7375c4ae893c4c670cb8e276b44588115f7
ventiv <108926711+ventiv@users.noreply.github.com>
2024-04-16 08:02:17 +0000

    1

---
+++ hello_console/lib/bingfa/1.dart
@@ -0,0 +1,16 @@
+const String filename = 'with_keys.json';
+
+void main() {
+  // Read some data.
+  final fileData = _readFileSync();
+  final jsonData = jsonDecode(fileData);
+
+  // Use that data.
+  print('Number of JSON keys: ${jsonData.length}');
+}
+
+String _readFileSync() {
+  final file = File(filename);
+  final contents = file.readAsStringSync();
+  return contents.trim();
+}
