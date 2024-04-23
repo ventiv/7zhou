f48ea0a31440289f535139f596ffc3a0c025a10f
ventiv <108926711+ventiv@users.noreply.github.com>
2024-04-23 06:50:29 +0000

    1

--- hello_console/bin/hello_console.dart
+++ hello_console/bin/hello_console.dart
@@ -1,5 +1,51 @@
-import 'package:hello_console/hello_console.dart' as hello_console;
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:args/args.dart';
+
+const lineNumber = 'line-number';
 
 void main(List<String> arguments) {
-  print('Hello world: ${hello_console.calculate()}!');
+  print(arguments);
+
+  exitCode = 0; // Presume success
+
+  final parser = ArgParser()..addFlag(lineNumber, negatable: false, abbr: 'n');
+
+  ArgResults argResults = parser.parse(arguments);
+  final paths = argResults.rest;
+
+  print("done.");
+}
+
+Future<void> dcat(List<String> paths, {bool showLineNumbers = false}) async {
+  if (paths.isEmpty) {
+    // No files provided as arguments. Read from stdin and print each line.
+    await stdin.pipe(stdout);
+  } else {
+    for (final path in paths) {
+      var lineNumber = 1;
+      final lines = utf8.decoder
+          .bind(File(path).openRead())
+          .transform(const LineSplitter());
+      try {
+        await for (final line in lines) {
+          if (showLineNumbers) {
+            stdout.write('${lineNumber++} ');
+          }
+          stdout.writeln(line);
+        }
+      } catch (_) {
+        await _handleError(path);
+      }
+    }
+  }
+}
+
+Future<void> _handleError(String path) async {
+  if (await FileSystemEntity.isDirectory(path)) {
+    stderr.writeln('error: $path is a directory');
+  } else {
+    exitCode = 2;
+  }
 }
--- hello_console/lib/hello_console.dart
+++ hello_console/lib/hello_console.dart
@@ -1,6 +1,19 @@
 import 'dart:convert';
 import 'dart:io';
 
-void main(List<String> arguments){
-  print("done");
+import 'package:args/args.dart';
+
+const lineNumber = 'line-number';
+
+void main(List<String> arguments) {
+  print(arguments);
+
+  exitCode = 0; // Presume success
+
+  final parser = ArgParser()..addFlag(lineNumber, negatable: false, abbr: 'n');
+
+  ArgResults argResults = parser.parse(arguments);
+  final paths = argResults.rest;
+
+  print("done.");
 }
